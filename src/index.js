daf43f0d677d5e1e50e7df243ef69bf0c7fa7713
Kest Ablis <kerljohnr@gmail.com>
2018-12-23 19:11:51 -0500

    let % be mod; add exp operator; remove 9-digit cap

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 let btnNum = 'btn circle white-red';
-let pillNum = 'btn pill white-red';
 let btnOp = 'btn circle pink-white';
 
 function DisplayBox(props) {
@@ -20,7 +19,8 @@ class Calculator extends React.Component {
     curVal: '',
     preVal: '',
     disVal: '',
-    operator: ''
+    operator: '',
+    evaluated: false
   }
 
   constructor(props) {
@@ -30,14 +30,13 @@ class Calculator extends React.Component {
 
   clickNumber(number) {
     let cur = this.state.curVal;
-    let val = cur + number;
+    let val = this.state.evaluated ?  number : cur + number;
 
-    if (cur.length < 9) {
-      this.setState({
-        curVal: val,
-        disVal: val
-      });
-    }
+    this.setState({
+      curVal: val,
+      disVal: val,
+      evaluated: false
+    });
   }
 
   clear() {
@@ -65,14 +64,14 @@ class Calculator extends React.Component {
     if (cur && pre && op) {
       cur = eval(pre + op + cur);
       cur = Number.isInteger(cur) ? cur : cur.toFixed(2);
-      cur = cur.toString().length > 9 ? cur.toExponential(2) : cur;
       cur = cur.toString();
 
       this.setState({
         curVal: cur,
         disVal: cur,
         preVal: '',
-        operator: ''
+        operator: '',
+        evaluated: true
       })
     }
   }
@@ -89,49 +88,39 @@ class Calculator extends React.Component {
     }
   }
 
-  percent() {
-    let cur = this.state.curVal;
-    let pre = this.state.preVal;
-    if(pre && cur && !cur.includes('%')) {
-      this.setState({
-        curVal: Number(cur) * Number(pre) / 100,
-        disVal: cur + '%'
-      });
-    }
-  }
-
   render() {
     return (
       <div id="calculator" >
         <DisplayBox value={this.state.disVal}/ >
         <div id="row1" className="calc-row">
           <button className={btnOp}  onClick={() => this.clear()}>AC</button>
-          <button className={btnOp}  onClick={() => this.plusMinus()}>+/-</button>
-          <button className={btnOp}  onClick={() => this.percent()}>%</button>
-          <button className={btnOp}  onClick={() => this.operator('/')}>/</button>
+          <button className={btnOp}  onClick={() => this.operator('**')}>^</button>
+          <button className={btnOp}  onClick={() => this.operator('%')}>%</button>
+          <button className={btnOp}  onClick={() => this.operator('+')}>+</button>
         </div>
         <div id="row2" className="calc-row">
           <button className={btnNum} onClick={() => this.clickNumber('7')}>7</button>
           <button className={btnNum} onClick={() => this.clickNumber('8')}>8</button>
           <button className={btnNum} onClick={() => this.clickNumber('9')}>9</button>
-          <button className={btnOp}  onClick={() => this.operator('*')}>x</button>
+          <button className={btnOp}  onClick={() => this.operator('-')}>-</button>
         </div>
         <div id="row3" className="calc-row">
           <button className={btnNum} onClick={() => this.clickNumber('4')}>4</button>
           <button className={btnNum} onClick={() => this.clickNumber('5')}>5</button>
           <button className={btnNum} onClick={() => this.clickNumber('6')}>6</button>
-          <button className={btnOp}  onClick={() => this.operator('+')}>+</button>
+          <button className={btnOp}  onClick={() => this.operator('*')}>x</button>
         </div>
         <div id="row4" className="calc-row">
           <button className={btnNum} onClick={() => this.clickNumber('1')}>1</button>
           <button className={btnNum} onClick={() => this.clickNumber('2')}>2</button>
           <button className={btnNum} onClick={() => this.clickNumber('3')}>3</button>
-          <button className={btnOp}  onClick={() => this.operator('-')}>-</button>
+          <button className={btnOp}  onClick={() => this.operator('/')}>/</button>
         </div>
         <div id="row5" className="calc-row">
-          <button id="zero" className={pillNum} onClick={() => this.clickNumber('0')}>0</button>
+          <button className={btnNum} onClick={() => this.clickNumber('0')}>0</button>
           <button className={btnNum} onClick={() => this.clickNumber('.')}>.</button>
-          <button className={btnOp} onClick={() => this.equals()}>=</button>
+          <button className={btnOp}  onClick={() => this.plusMinus()}>+/-</button>
+          <button className={btnOp}  onClick={() => this.equals()}>=</button>
         </div>
       </div>
     );
